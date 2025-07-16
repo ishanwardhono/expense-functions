49815ae97ac4b138f8e3175c2274430bdebe3c71
ishanw-evermos <ishan.wardhono@evermos.com>
2025-07-16 18:34:15 +0700

    rename package

--- main.go
+++ main.go
@@ -4,13 +4,12 @@ import (
 	"log"
 
 	// Blank-import the function package so the init() runs
-	_ "github.com/ishanwardhono/gcp-functions/weekly-expense"
+	_ "github.com/ishanwardhono/gcp-functions/hello"
 
 	"github.com/GoogleCloudPlatform/functions-framework-go/funcframework"
 )
 
 func main() {
-
 	log.Println("Starting Functions Framework...")
 	if err := funcframework.StartHostPort("localhost", "8199"); err != nil {
 		log.Fatalf("funcframework.StartHostPort: %v\n", err)
